094c1cf47d18c5c11e8cb093c2baadbc0782622f
Jakii971 <izzudinlagi971@gmail.com>
2024-03-29 10:41:28 +0700

    selesai semua

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
   async function makeList() {
+    document.body.style.overflow = 'hidden';
+
     const { value: text } = await Swal.fire({
       input: "text",
       title: "Tulis nama kamu!!",
       inputPlaceholder: "Type your message here...",
       inputAttributes: {
         maxlength: 10,
-        "aria-label": "Type your message here"
+        "aria-label": "Type your message here",
       },
     });
 
-    if(text){
+    if (text) {
       Swal.fire({
         title: `Salam kenal ${text}`,
-        text: 'Follow IG for more!! ig: @jak_zaki',
+        text: "Follow IG for more!! ig: @jak_zaki",
         icon: "success",
-        footer: '<a href="https://www.instagram.com/jak_zaki" target="_blank">Follow!!</a>'
+        footer:
+          '<a href="https://www.instagram.com/jak_zaki" target="_blank">Follow!!</a>',
       });
 
-      const friendList = document.getElementById('friend');
-  
+      const friendList = document.getElementById("friend");
+
       const textList = document.createElement("li");
       textList.innerText = text;
-  
+      
+      textList.classList.add('animate__animated', 'animate__bounceIn');
       friendList.appendChild(textList);
+      document.body.style.overflow = 'auto';
     }
   }
 
-  document.getElementById("addFriendButton").addEventListener("click", function(){
-    makeList()
-  });
+  document
+    .getElementById("addFriendButton")
+    .addEventListener("click", function () {
+      makeList();
+    });
 });
+
